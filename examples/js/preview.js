39cc3fb98090b4caa75c4ca30310a2eed74258b1
Adam Becker <ad@mbecker.cc>
2015-05-19 11:56:20 -0400

    no more noconflict in preview

--- examples/js/preview.js
+++ examples/js/preview.js
@@ -1,28 +1,28 @@
-jQuery('.config_select').each(function(){
-  var stored = store.get(jQuery(this).attr('id'));
+$('.config_select').each(function(){
+  var stored = store.get($(this).attr('id'));
 
   if (stored) {
-    jQuery(this).val(stored);
+    $(this).val(stored);
   } else {
-    jQuery(this).val(jQuery(this).find('option').first().val());
+    $(this).val($(this).find('option').first().val());
   }
 });
 
-jQuery('.config_select').change(function(){
-  store.set(jQuery(this).attr('id'), jQuery(this).val());
+$('.config_select').change(function(){
+  store.set($(this).attr('id'), $(this).val());
   location.reload();
 });
 
 // Load libraries
-jQuery('head').
-  append(jQuery('<link rel="stylesheet" type="text/css" />').attr('href', jQuery('#main').val())).
-  append(jQuery('<link rel="stylesheet" type="text/css" />').attr('href', jQuery('#lib').val()));
+$('head').
+  append($('<link rel="stylesheet" type="text/css" />').attr('href', $('#main').val())).
+  append($('<link rel="stylesheet" type="text/css" />').attr('href', $('#lib').val()));
 
 FormRenderer.BUTTON_CLASS = 'button button-primary btn btn-primary'
 
 // Initialize form
-var fr = new FormRenderer(jQuery.extend(
-  Fixtures.FormRendererOptions[jQuery('#fixture').val()](),
+var fr = new FormRenderer($.extend(
+  Fixtures.FormRendererOptions[$('#fixture').val()](),
   {
     screendoorBase: 'http://screendoor.dobt.dev',
     onReady: function(){

56d6d400717a6807eba9dce2836288377f37dc08
Adam Becker <ad@mbecker.cc>
2016-04-04 17:32:01 -0400

    required partial

--- dist/formrenderer.uncompressed.js
+++ dist/formrenderer.uncompressed.js
@@ -8790,6 +8790,8 @@ window.JST["fields/confirm"] = function(__obj) {
     
       _print(this.model.get('label'));
     
+      _print(_safe(JST["partials/required"](this)));
+    
       _print(_safe('\n</label>\n'));
     
     }).call(this);
@@ -10127,9 +10129,7 @@ window.JST["partials/label"] = function(__obj) {
     
       _print(this.model.get('label'));
     
-      if (this.model.get('required')) {
-        _print(_safe('&nbsp;<abbr class=\'fr_required\' title=\'required\'>*</abbr>'));
-      }
+      _print(_safe(JST["partials/required"](this)));
     
       _print(_safe('\n  '));
     
@@ -10529,6 +10529,55 @@ window.JST["partials/pagination"] = function(__obj) {
           _print(_safe('\n      </li>\n    '));
         }
         _print(_safe('\n  </ul>\n'));
+      }
+    
+      _print(_safe('\n'));
+    
+    }).call(this);
+    
+    return __out.join('');
+  }).call((function() {
+    var obj = {
+      escape: function(value) {
+        return ('' + value)
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;');
+      },
+      safe: _safe
+    }, key;
+    for (key in __obj) obj[key] = __obj[key];
+    return obj;
+  })());
+};
+
+if (!window.JST) {
+  window.JST = {};
+}
+window.JST["partials/required"] = function(__obj) {
+  var _safe = function(value) {
+    if (typeof value === 'undefined' && value == null)
+      value = '';
+    var result = new String(value);
+    result.ecoSafe = true;
+    return result;
+  };
+  return (function() {
+    var __out = [], __self = this, _print = function(value) {
+      if (typeof value !== 'undefined' && value != null)
+        __out.push(value.ecoSafe ? value : __self.escape(value));
+    }, _capture = function(callback) {
+      var out = __out, result;
+      __out = [];
+      callback.call(this);
+      result = __out.join('');
+      __out = out;
+      return _safe(result);
+    };
+    (function() {
+      if (this.model.get('required')) {
+        _print(_safe('&nbsp;<abbr class=\'fr_required\' title=\'required\'>*</abbr>'));
       }
     
       _print(_safe('\n'));

ca7e7066243f056916ef60ba50c03ba958643332
Andrew Brown <drusepth@gmail.com>
2017-06-29 19:05:30 +0100

    Rebuild all assets

--- dist/formrenderer.standalone.uncompressed.js
+++ dist/formrenderer.standalone.uncompressed.js
@@ -2710,7 +2710,7 @@ window.JST["fields/dropdown"] = function(__obj) {
       _print(_safe('" data-rv-value=\'model.value\'>\n  '));
     
       if (this.model.get('include_blank_option')) {
-        _print(_safe('\n    <option selected disabled class="hidden_option">\n      '));
+        _print(_safe('\n    <option selected>\n      '));
         _print(FormRenderer.t.choose_an_option);
         _print(_safe('\n    </option>\n  '));
       }

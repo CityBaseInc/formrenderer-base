36178b4862a77ee7764c02d077f6a784c4f304d2
Andrew Brown <andrew@dobt.co>
2018-08-30 18:13:48 -0500

    build dist/

--- dist/formrenderer.standalone.uncompressed.js
+++ dist/formrenderer.standalone.uncompressed.js
@@ -99,6 +99,7 @@ rivets.configure({
       responderLanguage: void 0,
       preview: false,
       skipValidation: void 0,
+      skipConditions: void 0,
       saveParams: {},
       showLabels: false,
       scrollToPadding: 0,
@@ -730,11 +731,14 @@ rivets.configure({
     };
 
     ConditionChecker.prototype.isVisible = function() {
-      var _ref;
+      var _ref, _ref1;
+      if ((_ref = this.responseField.fr.options) != null ? _ref.skipConditions : void 0) {
+        return true;
+      }
       if (!this.isValid()) {
         return true;
       }
-      if (_ref = this.condition.method, __indexOf.call(presenceMethods, _ref) >= 0) {
+      if (_ref1 = this.condition.method, __indexOf.call(presenceMethods, _ref1) >= 0) {
         return this["method_" + this.condition.method]();
       } else {
         return this.method_present() && this["method_" + this.condition.method]();
